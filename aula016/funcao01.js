e9ba9948391f5e1ce3ff76ef0e206225964a2140
Tifany Trindade <99377193+TifanyTrindade@users.noreply.github.com>
2025-02-28 00:13:22 -0300

    Exercicio

--- aula016/funcao01.js
+++ aula016/funcao01.js
@@ -1,3 +1,4 @@
+/*
 function parimpar(n) {
     if(n%2 == 0) {
         return 'Par!'
@@ -7,4 +8,15 @@ function parimpar(n) {
 }
 
 let res = parimpar(9)
+console.log(res)
+*/
+
+function idade(i){
+    if(i >= 18) {
+        return 'Maior de idade'
+    } else {
+        return 'Menor de idade'
+    }
+}
+let res = idade(18)
 console.log(res)
